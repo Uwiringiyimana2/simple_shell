472be429f83e46130bc902404d7b463019cb5c45
tuyisenge250 <benjaminwell250@gmail.com>
2023-11-08 04:25:35 -0800

    ben&eric shell

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 
 int main(int ac, char **argv)
 {
-	char *line = NULL;
+	char *line = NULL, *linecopy = NULL;
 	size_t n = 0;
 	ssize_t read;
 	char *prompt = {"Eric&BenShell$ "};
+	char *delim = '\0';
+	int num_token;
+	char *token;
 
 	/*declaring void variables*/
 	(void)ac; (void)argv;
@@ -20,15 +23,35 @@ int main(int ac, char **argv)
 		if (read == -1)
 		{
 			perror("getline error");
-			return (1);
+			return (-1);
 		}
+		linecopy = malloc(sizeof(char) * read);
+		if (linecopy = NULL)
+		{
+			ptintf("menory is empty errror in copy");
+			return(-1);
+		}
+		strcpy (linecopy,line);
 
-		/*tokenize the user input*/
+		token = strtok(line,delim);
+		while (token != NULL)
+		{
+			num_token++;
+			strtok(NULL,delim);
+		}num_token++;
+		argv[] = malloc (sizeof(char*) * num_token);
+		/*storing each token in it arry of argv*/
+		for (i = 0; token == NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
 
+			token = strtok(NULL, delim);
+		}
+		argv[i] = NULL;
 
-		printf("%s\n", line);
-
+		execmd(argv);
 	}
+	free(linecopy);
 	free(line);
-	return (0);
-}
+	return(0);
