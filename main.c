81138f973e536ca3c7668afe3d6c4a74a52f6f71
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-11-08 21:19:22 +0200

    shell

--- main.c
+++ main.c
@@ -6,18 +6,29 @@ int main(int ac, char **argv)
 	size_t n = 0;
 	ssize_t read;
 	char *prompt = {"Eric&BenShell$ "};
-	char *delim = '\0';
-	int num_token;
+	char *delim = " \0";
+	int num_token = 0;
 	char *token;
+	int i;
 
 	/*declaring void variables*/
-	(void)ac; (void)argv;
+	(void)ac;
 
 	while (1)
 	{
 		printf("%s", prompt);
-	
+
 		read = getline(&line, &n, stdin);
+
+		linecopy = malloc(sizeof(char) * read);
+
+		if (linecopy == NULL)
+		{
+			perror("menory is empty, errror in copy");
+			return(-1);
+															                 }
+
+		strcpy(linecopy, line);
 
 		/* check getline fail or reaches EOF*/
 		if (read == -1)
@@ -25,33 +36,36 @@ int main(int ac, char **argv)
 			perror("getline error");
 			return (-1);
 		}
-		linecopy = malloc(sizeof(char) * read);
-		if (linecopy = NULL)
+		else
 		{
-			ptintf("menory is empty errror in copy");
-			return(-1);
-		}
-		strcpy (linecopy,line);
+			token = strtok(line, delim);
 
-		token = strtok(line,delim);
-		while (token != NULL)
-		{
+			while (token != NULL)
+			{	
+				num_token++;
+				strtok(NULL, delim);
+			}
 			num_token++;
-			strtok(NULL,delim);
-		}num_token++;
-		argv[] = malloc (sizeof(char*) * num_token);
-		/*storing each token in it arry of argv*/
-		for (i = 0; token == NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char) * strlen(token));
-			strcpy(argv[i], token);
+		
+			argv[i] = malloc (sizeof(char*) * num_token);
 
-			token = strtok(NULL, delim);
+			/*storing each token in it arry of argv*/
+			for (i = 0; token != NULL; i++)
+			{
+				argv[i] = malloc(sizeof(char) * strlen(token));
+				strcpy(argv[i], token);
+
+				token = strtok(NULL, delim);
+			}
+			argv[i] = NULL;
 		}
-		argv[i] = NULL;
 
-		execmd(argv);
+		/*print the argv*/
+	
+		free(argv);
+        	free(linecopy);
+        	free(line);	 
 	}
-	free(linecopy);
-	free(line);
+
 	return(0);
+}
